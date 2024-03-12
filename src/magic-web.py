d56e138d33dd4c3bcd4829302c297a521f1561ce
EmmanuelPlatinum <141779528+EmmanuelPlatinum@users.noreply.github.com>
2024-03-12 10:49:43 -0400

    Update magic-web.py

--- src/magic-web.py
+++ src/magic-web.py
@@ -8,6 +8,14 @@ alunos = {}
 
 # Função para cadastrar professor
 def cadastrar_professor(nome, cpf, materia):
+    """
+    # cadastrar_professor():
+    Cadastra um novo professor no sistema.
+    Parâmetros:
+    - nome (str): Nome do professor a ser cadastrado.
+    - cpf (str): CPF do professor a ser cadastrado.
+    - materia (str): Matéria que o professor leciona.
+    """
     # Verificando se o professor já existe
     if cpf in professores:
         print(f"Professor com CPF {cpf} já cadastrado!")
@@ -24,6 +32,14 @@ def cadastrar_professor(nome, cpf, materia):
 
 # Função para cadastrar aluno
 def cadastrar_aluno(nome, cpf, professor_cpf):
+    """
+    cadastrar_aluno():
+    Cadastra um novo aluno no sistema.
+    Parâmetros:
+    - nome (str): Nome do aluno a ser cadastrado.
+    - cpf (str): CPF do aluno a ser cadastrado.
+    - professor_cpf (str): CPF do professor do aluno.
+    """
     # Verificando se o professor existe
     if professor_cpf not in professores:
         print(f"Professor com CPF {professor_cpf} não encontrado!")
@@ -46,6 +62,12 @@ def cadastrar_aluno(nome, cpf, professor_cpf):
     print(f"Aluno {nome} cadastrado com sucesso!")
 
 def carregar_dados_json():
+    """
+    carrgar_dados_json():
+    Carrega os dados de professores e alunos a partir de um arquivo JSON.
+    Retorna:
+    - tuple: Uma tupla contendo os dados dos professores e alunos carregados.
+    """
     with open("dados.json", "r") as arquivo:
         dados = json.load(arquivo)
 
@@ -53,8 +75,11 @@ def carregar_dados_json():
 
 # Função para listar alunos
 def listar_alunos():
+    """
+    Lista todos os alunos cadastrados no sistema.
+    """
     professores, alunos = carregar_dados_json()
-
+    
     for cpf, dados in alunos.items():
         print(f"CPF: {cpf}")
         print(f"Nome: {dados['nome']}")
@@ -62,6 +87,11 @@ def listar_alunos():
         print()
 
 def escolher_professor():
+    """
+    Solicita ao usuário que digite o CPF de um professor.
+    Retorna:
+    - str: CPF do professor escolhido.
+    """
     while True:
         cpf = input("Digite o CPF do professor: ")
         if cpf not in professores:
@@ -71,6 +101,14 @@ def escolher_professor():
             return cpf
 
 def listar_dados_professor(cpf):
+    """
+    Lista os dados de um professor específico.
+    Parâmetros:
+    - cpf (str): CPF do professor.
+
+    Retorna:
+    - bool: True se o professor foi encontrado e False caso contrário.
+    """
     if cpf not in professores:
         print(f"Professor com CPF {cpf} não encontrado!")
         return False
@@ -84,6 +122,9 @@ def listar_dados_professor(cpf):
     return True
 
 def listar_professores():
+    """
+    Lista todos os professores cadastrados no sistema.
+    """
     if not professores:
         print("Nenhum professor cadastrado.")
         return
@@ -96,6 +137,9 @@ def listar_professores():
         print()
 
 def listar_todos_alunos():
+    """
+    Lista todos os alunos, incluindo o nome do professor que eles pertencem.
+    """
     if not alunos:
         print("Nenhum aluno cadastrado.")
         return
@@ -108,6 +152,14 @@ def listar_todos_alunos():
         print()
 
 def deletar_professor():
+    """
+    deletar_professor():
+    Chama a funcao listar_professores().\n
+    Chama a funcao escolher_professor().\n
+    Recebe o CPF.\n
+    Busca professor por CPF.\n
+    Deletar professor.
+    """
     listar_professores()
     cpf_professor = escolher_professor()
     if cpf_professor:
@@ -119,6 +171,11 @@ def deletar_professor():
         print(f"Professor com CPF {cpf_professor} deletado com sucesso!")
 
 def deletar_aluno():
+    """
+    Deleta um aluno do sistema.
+    Este processo envolve selecionar um aluno da lista e removê-lo, 
+    juntamente com a atualização do professor associado a ele.
+    """
     listar_todos_alunos()
     cpf_aluno = input("Digite o CPF do aluno: ")
     if cpf_aluno in alunos:
